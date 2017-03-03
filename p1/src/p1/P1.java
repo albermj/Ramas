c8f4eac1bd617733b8cf47a746c67287e18a2adc
203p01 <203p01@203p23-PC>
2017-03-03 13:17:35 +0100

    Primera Version

---
+++ p1/src/p1/P1.java
@@ -0,0 +1,17 @@
+
+package p1;
+
+/**
+ *
+ * @author 203p01
+ */
+public class P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Version 1.0");
+    }
+    
+}
